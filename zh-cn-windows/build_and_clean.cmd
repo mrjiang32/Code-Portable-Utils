2ef8993309748b99d3aeb2d9e4cbb09916ae72b8
(●'◡'●) mrjiang32 <lingqunxie@gmail.com>
2025-02-19 05:46:09 +0000

    Update ``pyupdate.py`` and ``bulid_and_clean.cmd``
    support when code-tunnel not found.
    
    Signed-off-by: (●'◡'●) mrjiang32 <lingqunxie@gmail.com>

--- zh-cn-windows/build_and_clean.cmd
+++ zh-cn-windows/build_and_clean.cmd
@@ -1,6 +1,6 @@
 pip install -r requirement.txt
 pyinstaller --icon icon.ico --onefile --add-data "updates/7z.exe;updates" --add-data "updates/7z.dll;updates" --add-data "updates/7-zip.dll;updates" --add-data "updates/7-zip32.dll;updates" pyupdate.py
-move /Y dist\pyupdate.exe pyupdate.exe
+move /Y dist\pyupdate.exe codecore.exe
 rmdir dist
 del /Q pyupdate.spec
 rmdir /S /Q build
